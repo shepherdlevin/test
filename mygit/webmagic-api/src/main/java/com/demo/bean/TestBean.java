daa005cbc9079602324c4b38f1f490ed75f40d7a
wangwenlai <wwlai.2008@163.com>
2018-06-20 15:09:51 +0800

    init

---
+++ mygit/webmagic-api/src/main/java/com/demo/bean/TestBean.java
@@ -0,0 +1,17 @@
+package com.demo.bean;
+
+/**
+ * @author wenlai.wang
+ * @version 2017-06-15
+ */
+public class TestBean {
+    public int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
